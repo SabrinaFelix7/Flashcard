baa38b93ce3fc94494586f7390c914fce81e3f30
SabrinaFelix7 <167902896+SabrinaFelix7@users.noreply.github.com>
2024-11-14 22:59:23 -0300

    Add files via upload
    Projeto concluido flashcard

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Litertura',
+    'Qual é o romance mais famoso de Machado de Assis?',
+    '"Dom Casmurro"'
+)
+
+criaCartao(
+    'Adaptações literárias',
+    'Qual é o livro mais adaptado para o cinema?',
+    '"Orgulho e Precoceito", de Jane Austen'
+)
+
+criaCartao(
+    'Livros',
+    'Qual é o livro mais vendido de todos os tempos?',
+    'A Bíblia'
+)
+
+criaCartao(
+    'Lingua portuguesa',
+    'Quantas palavras existem a língua portuguesa?',
+    'Cerca de 400.000'
+)
